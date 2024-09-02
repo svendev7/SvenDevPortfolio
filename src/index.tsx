803a83f2f4c34444e0ab927bd1acc9abdff4b2d3
Aanvil7 <sven.groot@student.hu.nl>
2024-09-02 22:21:14 +0200

    some fixes for the main page

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from './app/pages/home';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <HomePage />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
